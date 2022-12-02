c0c0d3d3399b3ea734ec00aa758f64e25b21ebcf
Pradeep Puppala <119656229+PradeepKPuppala@users.noreply.github.com>
2022-12-02 18:28:59 +0530

    Update streamlit_app.py
    imported pandas
    added a csv file for panda to read the list of fruit items
    and streamlit library will display the items on the page

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,7 @@ streamlit.text('🥚🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+my-fruit-list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my-fruit-list)
